55d65107afc36a24a6ae87b224f3fe5b69969eec
nastyahh <nasstyahus@gmail.com>
2024-05-01 16:24:29 +0300

    calling the action of the "back" button

--- app/src/main/java/com/topic3/android/reddit/screens/ChooseCommunityScreen.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/ChooseCommunityScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.topic3.android.reddit.R
+import com.topic3.android.reddit.routing.BackButtonAction
 import com.topic3.android.reddit.routing.RedditRouter
 import com.topic3.android.reddit.viewmodel.MainViewModel
 import kotlinx.coroutines.Job
@@ -76,6 +77,9 @@ fun ChooseCommunityScreen(viewModel: MainViewModel, modifier: Modifier = Modifie
             )
         )
         SearchedCommunities(communities, viewModel, modifier)
+    }
+    BackButtonAction {
+        RedditRouter.goBack()
     }
 }
 
